f3ee23e1d03e0f63827de8958dce79b27d36d4f6
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-26 14:00:27 -0300

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Importação de biblioteca e variaveis de ambiente
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
@@ -8,9 +9,10 @@ const morgan = require("morgan");
 // Configuração do App
 const app = express();
 app.use(express.json()); // possibilita transsitar dados usando json
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
 
-
+// Configurações de acesso
+app.use(cors({ origin: "https://localhost:3000" }))
 
 // Configuração do Banco de Dados
 const { connection, authenticate } = require("./database/database")
@@ -30,11 +32,11 @@ app.use(rotasPets);
 
 
 // Escuta de eventos (listener)
-app.listen(3000, () => {
+app.listen(3001, () => {
     // gerar tableas a partir do model
     connection.sync();
 
     // force : true = resetar dados da tabela a cada atualização do codigo
     // connection.sync({ force: true });
-    console.log("Servidor rodando em http://localhost:3000");
+    console.log("Servidor rodando em http://localhost:3001");
 });
